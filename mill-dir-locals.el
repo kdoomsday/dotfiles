4292dc80e6f9386aa16ef8e5b84bf7ded9d0ddca
Eduardo Barrientos <kdoomsday@gmail.com>
2025-12-09 17:01:38 -0400

    Updated mill dir locals

--- mill-dir-locals.el
+++ mill-dir-locals.el
@@ -1,4 +1,5 @@
 ;; Dir locals for a Mill project. Rename to .dir-locals.el
-((scala-mode . ((projectile-project-run-cmd . "./mill --ticker false core.run")
+((scala-mode . ((projectile-project-type . 'mill)
+                (projectile-project-run-cmd . "./mill --ticker false core.run")
                 (projectile-project-compilation-cmd . "./mill --ticker false __.compile")
                 (projectile-project-test-cmd . "./mill --ticker false __.test"))))
